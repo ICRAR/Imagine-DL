8b4b3743b7cf2adcbddc2c669c2a28c965730a76
KevinVinsen <kvn1961@gmail.com>
2017-04-03 12:14:13 +0800

    Added the merge script

---
+++ casa_scripts/merge_measurement_sets.py
@@ -0,0 +1,32 @@
+#
+#    ICRAR - International Centre for Radio Astronomy Research
+#    (c) UWA - The University of Western Australia
+#    Copyright by UWA (in the framework of the ICRAR)
+#    All rights reserved
+#
+#    This library is free software; you can redistribute it and/or
+#    modify it under the terms of the GNU Lesser General Public
+#    License as published by the Free Software Foundation; either
+#    version 2.1 of the License, or (at your option) any later version.
+#
+#    This library is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with this library; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+#    MA 02111-1307  USA
+#
+"""
+Merge files
+"""
+import glob
+
+list_measurement_sets = []
+for file_name in glob.glob('/scratch/kevin/imagine'):
+    if file_name.endswith('.ms'):
+        list_measurement_sets.append(file_name)
+
+concat(list_measurement_sets, '/scratch/kevin/imagine/final.ms', timesort=False, copypointing=True)
